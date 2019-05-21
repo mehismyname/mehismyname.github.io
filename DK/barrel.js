211f88f8a4d0e16f96e07fc2ff83225b2175cd66
mehismyname <46386283+mehismyname@users.noreply.github.com>
2019-05-21 10:45:26 -0400

    Update barrel.js

--- DK/barrel.js
+++ DK/barrel.js
@@ -51,7 +51,7 @@ var BarrelScene = new Phaser.Class({
         this.tweens.add({
             targets: this.player,
             x:650,
-            duration:370,
+            duration:290,
             ease:'Expo.InOut',
             repeat:-1,
             yoyo:true,
